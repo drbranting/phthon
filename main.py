447b6417654e82751c394244509da684b24e501f
Darin <101472553+drbranting@users.noreply.github.com>
2022-03-12 11:16:22 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+x = int(5)   # x will be 5
+
+a = input("A: ")
+b = .15
+c = .20
+d = .25
+print (47.56 * a + 47.56)
+print (47.56 * b + 47.56)
+print (47.56 * c + 47.56)
+print (47.56 * d + 47.56)
+game1 = 13
+game2 = 18
+game3 = 21
+game4 = 17
+game5 = 31
+print ((game1+game2+game3+game4+game5)/5)
+x = input("Put value of x: ")
+print(x)
+x = input ("what is the bill?")
+print (.1 * x)
+print (.15 * x)
+print (.2 * x)
